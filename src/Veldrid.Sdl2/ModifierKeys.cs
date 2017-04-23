a6057bee026887db6fac097d0c70bff88d17bf08
Eric Mellino <erme@microsoft.com>
2017-04-23 14:35:09 -0700

    Move Sdl2Window into this project and clean things up a bit

---
+++ src/Veldrid.Sdl2/ModifierKeys.cs
@@ -0,0 +1,11 @@
+﻿namespace Veldrid.Sdl2
+{
+    [System.Flags]
+    public enum ModifierKeys
+    {
+        None = 0,
+        Alt = 1,
+        Control = 2,
+        Shift = 4,
+    }
+}
---
+++ src/Veldrid.Sdl2/WindowState.cs
@@ -0,0 +1,11 @@
+﻿namespace Veldrid.Sdl2
+{
+    public enum WindowState
+    {
+        Normal,
+        FullScreen,
+        Maximized,
+        Minimized,
+        BorderlessFullScreen
+    }
+}
