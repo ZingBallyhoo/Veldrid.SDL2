765e4e405afdb05979bc60e1a10b736cf6cde223
Eric Mellino <erme@microsoft.com>
2017-05-26 18:16:30 -0700

    Fix errors in TestApp

--- src/TestApp/Program.cs
+++ src/TestApp/Program.cs
@@ -20,24 +20,24 @@ namespace TestApp
 
             while (true)
             {
-                GL.ClearColor(Color.DarkBlue);
+                GL.ClearColor(0f, 0f, .75f, 1f);
                 GL.Viewport(0, 0, 1280, 720);
                 GL.Clear(ClearBufferMask.ColorBufferBit);
                 Sdl2Native.SDL_PumpEvents();
                 SDL_Event ev;
                 while (Sdl2Native.SDL_PollEvent(&ev) != 0)
                 {
-                    if (ev.type == SDL_EventType.SDL_WINDOWEVENT)
+                    if (ev.type == SDL_EventType.WindowEvent)
                     {
                         SDL_WindowEvent windowEvent = Unsafe.Read<SDL_WindowEvent>(&ev);
                         Console.WriteLine("Window event: " + windowEvent.@event);
                     }
-                    else if (ev.type == SDL_EventType.SDL_MOUSEMOTION)
+                    else if (ev.type == SDL_EventType.MouseMotion)
                     {
                         SDL_MouseMotionEvent mme = Unsafe.Read<SDL_MouseMotionEvent>(&ev);
                         Console.WriteLine($"X: {mme.x}, Y: {mme.y}, State: {mme.state}");
                     }
-                    else if (ev.type == SDL_EventType.SDL_QUIT)
+                    else if (ev.type == SDL_EventType.Quit)
                     {
                         Sdl2Native.SDL_DestroyWindow(window);
                         return;
